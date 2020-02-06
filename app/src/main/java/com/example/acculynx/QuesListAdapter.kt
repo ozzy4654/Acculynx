16cb4185b1361d8ff8de27d488bca26370502b0c
OzanKalan <ozan.kalan1@gmail.com>
2020-02-05 20:02:42 -0600

    created repro and commited blank app

---
+++ app/src/main/java/com/example/acculynx/QuesListAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.acculynx
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class QuesListAdapter : RecyclerView.Adapter<QuesViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuesViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: QuesViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
+
+class QuesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+}
