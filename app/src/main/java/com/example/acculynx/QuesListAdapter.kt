ddfd8b803a8d57b14117cb55c1ab07cea0568ae0
OzanKalan <ozan.kalan1@gmail.com>
2020-02-05 20:25:51 -0600

    create data classes for stackoverflow question queries

--- app/src/main/java/com/example/acculynx/QuesListAdapter.kt
+++ app/src/main/java/com/example/acculynx/QuesListAdapter.kt
@@ -1,13 +1,16 @@
 package com.example.acculynx
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 
-class QuesListAdapter : RecyclerView.Adapter<QuesViewHolder>() {
+class QuesListAdapter(private val activity: AppCompatActivity, private val quesList : ArrayList<StackOverFlowQuestions>) : RecyclerView.Adapter<QuesViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuesViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return QuesViewHolder(LayoutInflater.from(activity)
+            .inflate(R.layout.ques_list_item, parent, false))
     }
 
     override fun getItemCount(): Int {
